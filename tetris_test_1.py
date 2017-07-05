c6214c77464d5165584542c5d9373e92ff161182
Brittney Johnson <bjohns@mit.edu>
2017-07-05 10:14:17 -0400

    Update tetris_test_1.py

--- tetris_test_1.py
+++ tetris_test_1.py
@@ -1,11 +1,11 @@
 from graphics import *
-import tetris_day1
+import tetris
 
 win = GraphWin("WTP Tetris", 300, 300)
 
-block = tetris_day1.Block([5,6],"blue")
-block2 = tetris_day1.Block([1,2],"green")
-block3 = tetris_day1.Block([7,4],"orange")
+block = tetris.Block([5,6],"blue")
+block2 = tetris.Block([1,2],"green")
+block3 = tetris.Block([7,4],"orange")
 block.draw(win)
 block2.draw(win)
 block3.draw(win)
