f8a950ef8d546c26609c93b4b9ec3a74d5c951e9
Kristen Mabry <kmabry@mit.edu>
2017-07-10 15:06:18 -0400

    Tetris Day 4

--- tetris.py
+++ tetris.py
@@ -5,6 +5,7 @@
 
 from graphics import *
 import time
+import random
 
 #keeps track of variables for the game
 class Game():
@@ -13,42 +14,50 @@ class Game():
 		self.row = row
 		self.window = GraphWin("Tetris", col*30, row*30)
 		self.current_shape = ""
+		self.block_locations = [self.row]*self.column
 
-
-	def get_window(self):
-		x = self.column*30
-		y = self.row*30
-		return x,y
-	
+# creates a new shape and drops it until it can't move
 	def add_drop_shape(self,shape_letter):
-		if shape_letter == "I":
+		if shape_letter == 0:
 			self.current_shape = I_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "J":
+		elif shape_letter == 1:
 			self.current_shape = J_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "L":
+		elif shape_letter == 2:
 			self.current_shape = L_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "O":
+		elif shape_letter == 3:
 			self.current_shape = O_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "S":
+		elif shape_letter == 4:
 			self.current_shape = S_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "T":
+		elif shape_letter == 5:
 			self.current_shape = T_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
-		elif shape_letter == "Z":
+		elif shape_letter == 6:
 			self.current_shape = Z_shape([int(self.column/2),-1])
 			self.current_shape.draw(self.window)
 		while self.current_shape.can_move(0,1) == True:
 			self.current_shape.move(0,1)
-			time.sleep(1)
-		
+			time.sleep(.25)
 
+#replaces the block list with the location of each of the top blocks
+		for block in self.current_shape.list_of_blocks:
+			point = block.getP1()
+			y = point.getY()
+			x = point.getX()
+			if y/30 < self.block_locations[int(x/30)]:
+				self.block_locations[int(x/30)] = int(y/30)
+			
+	
+# creates a random shape and saves it as an instance variable
+	def random_shape(self):
+		self.current_shape = random.randint(0, 6)
+		return self.current_shape
 				
-game = Game(12,20)
+game = Game(10,20)
 		
 # creates a single block using the rectangle class in graphics
 class Block(Rectangle):
@@ -59,18 +68,18 @@ class Block(Rectangle):
 		Rectangle.setFill(self, color)
 		Rectangle.setWidth(self, 3)
 		
-	
+# for y, checks if the space below it is unoccupied based on the list of current top block locations
+# need to fix x
 	def can_move(self,dx, dy):
-		win_x, win_y = game.get_window()
 		bottom_right = Rectangle.getP2(self)
 		right_x = bottom_right.getX()
 		lowest_y = bottom_right.getY()
-		if lowest_y + dy*30 > win_y:
+		if (lowest_y/30)-1 + dy >= game.block_locations[int((right_x/30)-1)]:
 			can_move = False
 		elif dx != 0:
-			if dx > 0 and right_x + dx*30 > win_x:
+			if dx > 0 and right_x/30 + dx > win_x:
 				can_move = False
-			if dx < 0 and (right_x-30) - dx*30 < 0:
+			if dx < 0 and (right_x-30)/30 - dx < 0:
 				can_move = False
 		else:
 			can_move = True
@@ -163,7 +172,10 @@ class Z_shape(Shape):
 		block_3_coords = [center_coords[0]-1, center_coords[1]]
 		list_of_coords = [block_1_coords, center_coords, block_2_coords, block_3_coords]
 		Shape.__init__(self, list_of_coords, "magenta")
-	
 
-"""shape = T_shape([3,5])
-print(shape.can_move(-3,5))"""
+def drop_shapes() :
+	while 0 not in game.block_locations:
+		new_shape = game.random_shape()
+		game.add_drop_shape(new_shape)
+		time.sleep(1)
+drop_shapes()
